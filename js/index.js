1b5007141c9c7f668c908ee47e4d7ade4a93ea80
GeorgeKahugu <gee.kahugu@gmail.com>
2024-04-09 16:51:41 +0300

    formatted index.js

--- js/index.js
+++ js/index.js
@@ -1,32 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
-    
-document.getElementById('shortenUrlBtn').addEventListener('click', function() {
-   
-    const longUrl = document.getElementById('longUrlInput').value.trim();
-    if (longUrl === '') {
-        alert('Please enter a valid long URL.');
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("shortenUrlBtn")
+    .addEventListener("click", function () {
+      const longUrl = document.getElementById("longUrlInput").value.trim();
+      if (longUrl === "") {
+        alert("Please enter a valid long URL.");
         return;
-    }
-    
-    // Make a POST request to CleanURI API to shorten the URL
-    fetch('https://cleanuri.com/api/v1/shorten', {
-        method: 'POST',
+      }
+
+      // Make a POST request to CleanURI API to shorten the URL
+      fetch("https://cleanuri.com/api/v1/shorten", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ url: longUrl })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.result_url) {
-            document.getElementById('result').innerHTML = `<p>Shortened URL: <a href="${data.result_url}" target="_blank">${data.result_url}</a></p>`;
-        } else {
-            alert('Failed to shorten URL. Please try again.');
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('An error occurred while shortening the URL. Please try again later.');
+        body: JSON.stringify({ url: longUrl }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.result_url) {
+            document.getElementById(
+              "result"
+            ).innerHTML = `<p>Shortened URL: <a href="${data.result_url}" target="_blank">${data.result_url}</a></p>`;
+          } else {
+            alert("Failed to shorten URL. Please try again.");
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          alert(
+            "An error occurred while shortening the URL. Please try again later."
+          );
+        });
     });
 });
-})
